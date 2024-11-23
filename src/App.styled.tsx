49fa746c2d77b62dcedf33004712ec5e589ad4ab
Hafsa Hameed <hhameed.bese-26mcs@student.nust.edu.pk>
2024-11-23 12:35:00 +0500

    body styling done

---
+++ src/App.styled.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const MyDiv = styled.div`
+  width: 100vw;
+  height: 100dvh;
+`;
+
+// @media (max-width: 768px) {
+//     padding: 20px;
+//   }
+//   @media (max-width: 480px) {
+//     padding: 10px;
+//   }

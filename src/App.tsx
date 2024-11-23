8227f98e8b4c5cedc19926762b4e2f679dd8f79c
Hafsa Hameed <hhameed.bese-26mcs@student.nust.edu.pk>
2024-11-23 15:51:04 +0500

    home pGE DONE

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import "./App.css";
 import { MyDiv } from "./App.styled";
+import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
-
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 function App() {
   return (
-    <>
+    <Router>
       <MyDiv>
         <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<div />} />
+          <Route path="/contact" element={<div />} />
+          <Route path="/solution" element={<div />} />
+          <Route path="/insights" element={<div />} />
+        </Routes>
       </MyDiv>
-    </>
+    </Router>
   );
 }
 
--- src/components/Navbar/Navbar.styled.tsx
+++ src/components/Navbar/Navbar.styled.tsx
@@ -1,32 +1,47 @@
 import styled from "styled-components";
 
-export const NavbarContainer = styled.div`
+export const NavbarContainer: React.FC<any> = styled.div`
   display: flex;
-  justify-content: space-between;
-
-  align-items: center;
-  height: 80px;
   font-size: 1.2rem;
-  margin-top: 1rem;
+  height: 80px;
+
   padding: 0 5rem;
+  align-items: center;
   @media (max-width: 768px) {
     padding: 20px;
+    height: auto;
   }
   @media (max-width: 480px) {
     padding: 10px;
+    height: auto;
+  }
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(
+      255,
+      255,
+      255,
+      0.5
+    ); /* White color with 50% opacity */
+    z-index: -1;
   }
 `;
 
 export const NavLink = styled.a`
   color: #19124f;
 
-  font-size: 24px;
+  font-size: 20px;
 
   font-weight: 500;
 
   cursor: pointer;
   text-decoration: none;
-  font-style: italic;
+  font-style: italics;
   @media (max-width: 480px) {
     font-size: 18px;
   }
